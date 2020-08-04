fee79abe454606bb748e68c742c3acd3d3f4041b
Chadwick Geyser <borborygmus8@gmail.com>
2020-08-04 10:35:32 -0500

    DOMMMMMMM

---
+++ script.js
@@ -0,0 +1,52 @@
+window.addEventListener('DOMContentLoaded', () => {
+    const button = document.createElement('button');
+    button.textContent = 'Add Square';
+    document.body.appendChild(button);
+
+    let numDivs = 0;
+
+    button.addEventListener('click', () => {
+        const div = document.createElement('div'),
+            span = document.createElement('span');
+
+        div.className = 'div';
+        div.id = numDivs;
+        span.textContent = div.id;
+        span.style.display = 'none';
+
+        div.addEventListener('mouseover', () => {
+            span.style.display = 'flex';
+        });
+
+        div.addEventListener('mouseout', () => {
+            span.style.display = 'none';
+        });
+
+        div.addEventListener('click', () => {
+            const color = `rgb(${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)})`;
+            div.style.backgroundColor = color;
+        });
+
+        div.addEventListener('dblclick', e => {
+            if (div.getAttribute('id') % 2 === 0) {
+                if (e.target.nextElementSibling) {
+                    document.body.removeChild(e.target.nextElementSibling);
+                    numDivs--;
+                } else {
+                    alert('No next element sibling to remove!');
+                }
+            } else {
+                if (e.target.previousElementSibling && e.target.previousElementSibling.classList.contains('div')) {
+                    document.body.removeChild(e.target.previousElementSibling);
+                    numDivs--;
+                } else {
+                    alert('No previous element sibling to remove!');
+                }
+            }
+        });
+
+        numDivs++;
+        div.appendChild(span);
+        document.body.appendChild(div);
+    });
+});
